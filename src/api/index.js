185a53511998eb05013ee033810b0b451d8034af
Swoyen <Swoyen>
2021-07-05 19:23:28 +0930

    Cors policy

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const headers = {
-  "Ocp-Apim-Subscription-Key": "1930c3ce89ec4636b5916f96e2141167",
+  "Ocp-Apim-Subscription-Key": "7b1788f9dd9945aa83699487bea4d6f4",
 };
 
-const BASE_URL = "https://restaurantapi.azure-api.net/api/";
+const BASE_URL = "https://restauapi.azurewebsites.net/api/";
 
 export const ENDPOINTS = {
   CUSTOMER: "Customer",
